4e3b6f6414335d78856447a95c1d83c723ab7a86
Sergio Vago <sergio.vago@hotmail.com>
2020-05-04 20:02:23 -0300

    backend deletado e arquivos movidos para fora

--- backend/start/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
-
-Route.resource('funds', 'FundController').apiOnly()
-Route.get('funds/aux/cnpj/:cnpj', 'AuxFundsController.show')
-Route.get('funds/aux/updateAll/', 'AuxFundsController.updateAll')
