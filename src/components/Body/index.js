dc9742d4e9eef17d43080e728a5bedf539bf876d
Sergio Vago <sergio.vago@hotmail.com>
2020-05-04 23:07:19 -0300

    Apenas 1 fundo pode ser adicionado

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -10,15 +10,32 @@ import './styles.css'
 export default props => {
   const [funds, setFunds] = useState([])
   const [fundsCards, setFundsCards] = useState([])
-
+  const [display, setDisplay] = useState("none")
 
   function handleSearchBarChange(selectedOption) {
-    const filteredFund = funds.filter(fund => {
+    const tem = fundsCards.filter(fund => {
       return fund.CNPJ === selectedOption.value
     })
 
-    console.log('filteredFund', filteredFund)
-    setFundsCards([filteredFund[0], ...fundsCards])
+    if (tem.length === 0) {
+      handleHideError(false)
+
+      const filteredFund = funds.filter(fund => {
+        return fund.CNPJ === selectedOption.value
+      })
+
+      setFundsCards([filteredFund[0], ...fundsCards])
+    } else {
+      handleHideError(true)
+    }
+  }
+
+  const handleHideError = (show) => {
+    if (show) {
+      setDisplay("inline")
+    } else {
+      setDisplay("none")
+    }
   }
 
   useEffect(() => {
@@ -32,8 +49,9 @@ export default props => {
     <div id="container">
       <h1>Buscar</h1>
       <p>Encontre o valor <span className="hightlight">atualizado</span> da sua cota</p>
-      <section className="searchs" >
+      <section className="searchs">
         <SearchBar Funds={funds} handleOnChange={handleSearchBarChange} />
+        <span style={{ color: '#DB340B', display }}>Essa cota já foi adicionada . . .</span>
       </section>
 
       <Cards Funds={fundsCards}></Cards>
